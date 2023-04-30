07e74211fe862f029ad01f4c7d7b47dbadbdbb6e
Dmitri <96867716+DmitriPrilucki@users.noreply.github.com>
2023-04-30 19:59:23 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37"
+
+let item = "";
+
+let btn1 = document.getElementById('btn1');
+
+btn1.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText('You choose 1st production');
+        item = '1';
+        tg.MainButton.show();
+    }
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(item);
+});
+
+
+let usercard = document.getElementById('usercard');
+let p = document.createElement('p')
+p.innerText = `${tg.initDataUnsafe.first_name}
+${tg.initDataUnsafe.last_name}`
+
+usercard.appendChild(p)
